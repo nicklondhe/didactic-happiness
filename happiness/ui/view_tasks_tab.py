fb9605c1694f4da32ea3377e2777a2a047de3294
Nikhil Londhe <nicklondhe@gmail.com>
2024-11-10 16:53:17 -0800

    fix(ui): Adding some styling

--- happiness/ui/view_tasks_tab.py
+++ happiness/ui/view_tasks_tab.py
@@ -1,18 +1,29 @@
 '''View tasks tab layout'''
+import dash_bootstrap_components as dbc
 from dash import dash_table, html
 
-view_tasks_layout = html.Div([
-    html.Br(),
-    dash_table.DataTable(
-        id='tasks-table',
-        columns=[
-            {'name': 'Task Name', 'id': 'name'},
-            {'name': 'Complexity', 'id': 'complexity'},
-            {'name': 'Type', 'id': 'type'},
-            {'name': 'Priority', 'id': 'priority'},
-            {'name': 'Repeatable', 'id': 'repeatable'},
-            {'name': 'Status', 'id': 'status'}
-        ],
-        data=[]
-    )
+view_tasks_layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H3('View Tasks', className='text-center my-4'), width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(dash_table.DataTable(
+            id='tasks-table',
+            columns=[
+                {'name': 'Task Name', 'id': 'name', 'type': 'text'},
+                {'name': 'Complexity', 'id': 'complexity', 'type': 'text'},
+                {'name': 'Type', 'id': 'type', 'type': 'text'},
+                {'name': 'Priority', 'id': 'priority', 'type': 'text'},
+                {'name': 'Repeatable', 'id': 'repeatable', 'type': 'text'},
+                {'name': 'Status', 'id': 'status', 'type': 'text'}
+            ],
+            page_current=0,
+            page_size=10,
+            page_action='native',
+            sort_action='native',
+            filter_action='native',
+            style_table={'overflowX': 'auto'},
+            style_cell={'backgroundColor': 'rgb(50, 50, 50)', 'color': 'white'},
+        ), width='auto', className='dbc')
+    ])
 ])
--- happiness/ui/workflow_tab.py
+++ happiness/ui/workflow_tab.py
@@ -1,36 +1,41 @@
 '''Task workflow tab layout'''
+import dash_bootstrap_components as dbc
 from dash import dash_table, html
 
-workflow_layout = html.Div([
-    html.Br(),
-    html.Div([
-        html.Button('Regenerate', id='regenerate', n_clicks=0),
-        html.Div(id='workflow-output')
+workflow_layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H3('Workflow', className='text-center my-4'), width=12)
     ]),
-    dash_table.DataTable(
-        id='recommended-tasks-table',
-        columns=[
-            {'name': 'Task Name', 'id': 'name'},
-            {'name': 'Type', 'id': 'type'},
-            {'name': 'Priority', 'id': 'priority'},
-            {'name': 'Rating', 'id': 'rating', 'presentation': 'dropdown', 'editable': True},
-        ],
-        data=[],
-        row_selectable='single',
-        selected_rows=[],
-        hidden_columns=['task_id', 'rec_id'],
-        css=[{"selector": ".show-hide", "rule": "display: none"}],
-        dropdown={
-            'rating': {
-                'options': [
-                    {'label': str(i), 'value': i} for i in range(1, 6)
-                ]
-            }
-        }
-    ),
-    html.Div([
-        html.Button('Start Task', id='start-task', n_clicks=0),
-        html.Button('Stop Task', id='stop-task', n_clicks=0),
-        html.Button('End Task', id='end-task', n_clicks=0),
+    dbc.Row([
+        dbc.Col(dash_table.DataTable(
+            id='recommended-tasks-table',
+            columns=[
+                {'name': 'Task ID', 'id': 'task_id'},
+                {'name': 'Recommendation ID', 'id': 'rec_id'},
+                {'name': 'Name', 'id': 'name'},
+                {'name': 'Type', 'id': 'type'},
+                {'name': 'Priority', 'id': 'priority'}
+            ],
+            data=[],
+            row_selectable='single',
+            style_table={'overflowX': 'auto'},
+            style_header={'backgroundColor': 'rgb(30, 30, 30)', 'color': 'white'},
+            style_cell={'backgroundColor': 'rgb(50, 50, 50)', 'color': 'white'},
+            hidden_columns=['task_id', 'rec_id'],
+            css=[{"selector": ".show-hide", "rule": "display: none"}],
+        ), width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(dbc.Button(
+            'Regenerate', id='regenerate', color='primary', className='mr-2'), width='auto'),
+        dbc.Col(dbc.Button(
+            'Start Task', id='start-task', color='success', className='mr-2'), width='auto'),
+        dbc.Col(dbc.Button(
+            'Stop Task', id='stop-task', color='warning', className='mr-2'), width='auto'),
+        dbc.Col(dbc.Button(
+            'End Task', id='end-task', color='danger'), width='auto')
+    ], className='my-4'),
+    dbc.Row([
+        dbc.Col(html.Div(id='workflow-output', className='mt-4'), width=12)
     ])
 ])
