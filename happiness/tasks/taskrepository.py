87c3e3ec1e4055d2675b548e03cdb6b8178d6005
Nikhil Londhe <nicklondhe@gmail.com>
2024-12-05 18:33:50 -0800

    feat(task cache): Cache tasks (untested)

--- happiness/tasks/taskrepository.py
+++ happiness/tasks/taskrepository.py
@@ -16,6 +16,7 @@ class TaskRepository:
         '''Initialize task repository'''
         self._db_session = db_session
         self._recommender = RandomRecommender()
+        self.tasks = None
         logger.info(f'Init task repository with a {self._recommender.__class__} recommender')
 
     def add_task(self, task: TaskWrapper) -> None:
@@ -29,11 +30,18 @@ class TaskRepository:
         )
         self._db_session.add(new_task)
         self._db_session.commit()
+        self.get_tasks() # only to load
+        self.tasks.append(new_task)
 
     def get_tasks(self) -> List[TaskWrapper]:
         '''Get all pending tasks'''
-        tasks = self._db_session.query(Task).filter(not_(Task.status == 'done')).all()
-        return [TaskWrapper(task) for task in tasks]
+        if not self.tasks:
+            logger.info('Tasks not loaded, doing a db fetch')
+            tasks = self._db_session.query(Task).filter(not_(Task.status == 'done')).all()
+            tasks = [TaskWrapper(task) for task in tasks]
+            logger.info(f'Loaded {len(tasks)} tasks)')
+            self.tasks = tasks
+        return self.tasks
 
     def recommend_tasks(self, num_tasks: int) -> List[TaskWrapper]:
         '''Recommend tasks based on user's mood'''
@@ -124,6 +132,7 @@ class TaskRepository:
             self._update_work_log(task_id, rec_id)
             self._update_task_summary(task_id)
             self._db_session.commit()
+            self.tasks.remove(task) # remove task from cache as cant be recommended
             return f'Task {task.name} started successfully!'
         except ValueError as err:
             logger.exception(err)
@@ -140,6 +149,7 @@ class TaskRepository:
             time_worked = (work_log.end_ts - work_log.start_ts).seconds
             self._update_task_summary(task_id, time_worked=time_worked)
             self._db_session.commit()
+            self.tasks.append(task) # add back to cache
             return f'Task {task.name} stopped successfully!'
         except ValueError as err:
             logger.exception(err)
@@ -157,6 +167,7 @@ class TaskRepository:
             self._update_task_summary(task_id, time_worked=time_worked,
                                       has_end_date=True, rating=rating)
             self._db_session.commit()
+            self.tasks.remove(task) # remove from cache
             return f'Task {task.name} finished successfully!'
         except ValueError as err:
             logger.exception(err)
