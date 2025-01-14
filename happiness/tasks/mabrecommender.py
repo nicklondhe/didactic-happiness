f3dfa57c0c450dd60aae51e1ab4ed45b1f719ed6
Nikhil Londhe <nicklondhe@gmail.com>
2025-01-14 14:32:06 -0800

    feat(cmad): Untested contextual MAB

--- happiness/tasks/mabrecommender.py
+++ happiness/tasks/mabrecommender.py
@@ -1,5 +1,6 @@
 '''A MAB based recommender for tasks'''
 from collections import defaultdict
+from datetime import datetime, timezone
 from typing import Dict, List
 import pickle
 import random
@@ -16,8 +17,10 @@ class MABRecommender(TaskRecommenderInterface):
         self.mdl_file = mdl_file
         self.qvalues, self.counts = self._load_model(self.mdl_file)
         self.epsilon = epsilon
+        self.last_context = None
         self.last_tasks = {} # last recs
         self.task_chosen = False
+        self.ce = ContextEncoder(6, 22, 4) #TODO: load from config?
         logger.info(f'Loaded MAB recommender with epsilon \
                     {self.epsilon} and {len(self.qvalues)} arms')
 
@@ -30,35 +33,16 @@ class MABRecommender(TaskRecommenderInterface):
 
         return data['qvalues'], data['counts']
 
-    def recommend_tasks(self, tasks: List[TaskWrapper], num_tasks: int) -> List[TaskWrapper]:
-        '''Recommend tasks based on MAB'''
-        if not self.task_chosen: # nothing from last set
-            self.update_qvalues()
-
-        hashed_tasks = self._as_hashed_tasks(tasks)
-        recs = []
-        arm_keys = set(hashed_tasks.keys())
-        arm_history = set()
-        qvalues = sorted((qv for qv in self.qvalues.items() if qv[0] in arm_keys),
-                         key = lambda x: x[1], reverse=True)
-        for k in arm_keys:
-            if k not in qvalues:
-                qvalues.append((k, 0))
-
-        while len(recs) < num_tasks:
-            if random.random() < self.epsilon:
-                available_arms = list(arm_keys - arm_history)
-                selected_arm = random.choice(available_arms)
-            else:
-                for arm, _ in qvalues:
-                    if arm not in arm_history:
-                        selected_arm = arm
-                        break
-            task = hashed_tasks[selected_arm][0]
-            recs.append(task)
-            arm_history.add(selected_arm)
-            self.last_tasks[task.get_id()] = task.get_hash_code()
-        return recs
+    def _load_contextual_values(self) -> dict:
+        '''Load contextual model based on time of day'''
+        curr_hr = datetime.now(timezone.utc).hour
+        ctx = self.ce.get_context(curr_hr)
+        self.last_context = ctx
+        ctx_qvalues = self.qvalues.get(ctx, None)
+        if not ctx_qvalues:
+            logger.error(f'Could not laod contextual values for {curr_hr}')
+            self.last_context = None
+        return ctx_qvalues
 
     def _as_hashed_tasks(self, tasks: List[TaskWrapper]) -> Dict[int, List[TaskWrapper]]:
         '''Convert tasks into a dict with hash as key and tasks as list'''
@@ -68,19 +52,78 @@ class MABRecommender(TaskRecommenderInterface):
             hashed_tasks[task.get_hash_code()].append(task)
         return hashed_tasks
 
-    def update_chosen_task(self, task_id: int) -> None:
-        self.task_chosen = True
-        self.update_qvalues(task_id)
-        return super().update_chosen_task(task_id)
+    @staticmethod
+    def _sort_and_augment_qvalues(qvalues: dict, task_keys: set) -> list:
+        '''Sort qvalues and augment with missing keys if any'''
+        missing_keys = task_keys - qvalues.keys()
+        sorted_qvalues = sorted((qv for qv in qvalues.items() if qv[0] in task_keys),
+                         key = lambda x: x[1], reverse=True)
+        for k in missing_keys:
+            sorted_qvalues.append((k, 0))
+        return sorted_qvalues
 
-    def update_qvalues(self, task_id: int = None) -> None:
+    def _run_mab(self, qvalues: Dict[int, float], hashed_tasks: Dict[int, List[TaskWrapper]], num_tasks: int) -> list:
+        '''Run multi arm bandit'''
+        recs = list()
+        arm_keys = hashed_tasks.keys()
+        sorted_qvalues = self._sort_and_augment_qvalues(qvalues, arm_keys)
+        arm_history = set()
+        while len(recs) < num_tasks:
+            task = None
+            if random.random() < self.epsilon:
+                # random pull
+                available_arms = list(arm_keys - arm_history)
+                selected_arm = random.choice(available_arms)
+            else:
+                # select based on qvalue
+                for arm, _ in sorted_qvalues:
+                    if arm not in arm_history:
+                        selected_arm = arm
+                        break
+            arm_history.add(selected_arm)
+            arm_tasks = hashed_tasks[selected_arm]
+            for t in arm_tasks:
+                if t.get_id() not in self.last_tasks:
+                    task = t
+                    break
+            if not task:
+                task = arm_tasks[0]
+                logger.warning(f'Using first task {task.get_name()} as no other tasks available')
+                
+            self.last_tasks[task.get_id()] = task.get_hash_code()
+            recs.append(task)
+
+        return recs
+
+    def _update_qvalues(self, task_id: int = None) -> None:
         '''Set reward as 1 for selected task id, and 0 for others'''
         for t_id, t_hash in self.last_tasks.items():
-            count = self.counts.get(t_hash, 0)
+            count = self.counts[self.last_context].get(t_hash, 0)
             count += 1
             reward = 1 if t_id == task_id else 0
-            self.qvalues[t_hash] += (reward - self.qvalues[t_hash]) * 1.0 / count
-            self.counts[t_hash] = count
+            self.qvalues[self.last_context][t_hash] += (reward - self.qvalues[self.last_context][t_hash]) * 1.0 / count
+            self.counts[self.last_context][t_hash] = count
+    
+    def new_recs(self, tasks: List[TaskWrapper], num_tasks: int) -> List[TaskWrapper]:
+        '''Contextual MAB recs'''
+        # Flush old recs and update qvalues, counts
+        if not self.task_chosen and self.last_context:
+            self._update_qvalues()
+
+        # load qvalues based on context
+        qvalues = self._load_contextual_values()
+        if qvalues:
+            hashed_tasks = self._as_hashed_tasks(tasks)
+            recs = self._run_mab(qvalues, hashed_tasks, num_tasks)
+            return recs
+        else:
+            logger.warning('Returning random tasks')
+            return tasks[:num_tasks]
+
+    def update_chosen_task(self, task_id: int) -> None:
+        self.task_chosen = True
+        self._update_qvalues(task_id)
+        return super().update_chosen_task(task_id)
 
     def load(self):
         '''Reload model'''
@@ -92,3 +135,26 @@ class MABRecommender(TaskRecommenderInterface):
             obj = {'qvalues': self.qvalues, 'counts': self.counts}
             pickle.dump(obj, f)
         return super().save()
+
+class ContextEncoder:
+    '''Encodes time of day into a numerical context'''
+    def __init__(self, start_bound: int, end_bound: int, duration: int):
+        '''Constructor'''
+        self.start_bound = start_bound + duration
+        self.end_bound = end_bound - duration
+        self.duration = duration
+        self.num_intervals = int((end_bound - start_bound) / duration)
+
+    def get_context(self, hr: int) -> int:
+        '''Get context based on hour'''
+        if hr < self.start_bound:
+            return 0
+        elif hr >= self.end_bound:
+            return int(self.num_intervals - 1)
+        else:
+            hr -= self.start_bound
+            return int(hr / self.duration) + 1
+
+    def get_num_intervals(self):
+        '''Get total number of intervals'''
+        return self.num_intervals
